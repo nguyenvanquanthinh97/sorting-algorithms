7daf70def465b8dc78ea81b09067a012ff662f2d
Thinh97 <nvq.thinh.hcmus.220597@gmail.com>
2020-09-02 22:08:10 +0700

    graph traversal

--- graph.js
+++ graph.js
@@ -34,8 +34,8 @@ class Graph {
 	removeEdge(v1, v2) {
 		this.adjacencyList[v1] = this.adjacencyList[v1].filter((vertex) => vertex !== v2);
 		this.adjacencyList[v2] = this.adjacencyList[v2].filter((vertex) => vertex !== v1);
-  }
-  /**
+	}
+	/**
    * This function help to remove vertex and its edges
    * @function removeVertex
    * @param {String, Number} vertex - vertex
@@ -44,6 +44,74 @@ class Graph {
 		const edges = this.adjacencyList[vertex];
 		edges.forEach((item) => this.removeEdge(vertex, item));
 		delete this.adjacencyList[vertex];
+	}
+	/**
+	 * This function traverse all vertex in graph with DFS recursively
+	 * @function dephthFirstRecursive
+	 * @param {String, Number} start - vertex
+	 * @returns {Array}
+	 */
+	dephthFirstRecursive(start) {
+		const result = [];
+		const visited = {};
+		const DFS = (vertex) => {
+			if (visited[vertex]) {
+				return;
+			}
+			visited[vertex] = true;
+			result.push(vertex);
+			this.adjacencyList[vertex].forEach((neighbor) => DFS(neighbor));
+		};
+		DFS(start);
+		return result;
+	}
+	/**
+	 * This function traverse all vertex in graph with DFS iteratively
+	 * @function depthFirstIterate
+	 * @param {String, Number} start - vertex
+	 * @returns {Array}
+	 */
+	depthFirstIterate(start) {
+		const result = [];
+		const visited = {};
+		const stack = [ start ];
+
+		let vertex;
+		while (stack.length) {
+			vertex = stack.pop();
+			if (visited[vertex]) continue;
+			visited[vertex] = true;
+			result.push(vertex);
+			stack.push(...this.adjacencyList[vertex]);
+		}
+		return result;
+	}
+	/**
+	 * This function traverse all vertex in graph with BFS iteratively
+	 * @function breadthFirstIterative
+	 * @param {String, Number} start - Vertex
+	 * @return {Array}
+	 */
+	breadthFirstIterative(start) {
+		const result = [];
+		const visited = {};
+
+		const queue = [ start ];
+		visited[start] = true;
+
+		let vertex;
+		while (queue.length) {
+			vertex = queue.shift();
+			result.push(vertex);
+
+			this.adjacencyList[vertex].forEach((neighbor) => {
+				if (!visited[neighbor]) {
+					visited[neighbor] = true;
+					queue.push(neighbor);
+				}
+			});
+		}
+		return result;
 	}
 }
 
